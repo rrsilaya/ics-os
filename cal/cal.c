ff071cbaac7cb2cc2d8c61ded381ab5dc149f365
rrsilaya <rrsilaya@up.edu.ph>
2018-05-01 21:39:59 +0800

    Fix issue on negative mod

--- cal/cal.c
+++ cal/cal.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include "cal.h"
 
 int main() {
-  // for (int i = 1; i <= 12; i++) {
-  //   printf("[Month %i]\n", i);
-  //   cal(i, 2017);
-  //   printf("\n");
-  // }
-  cal(12, 2019);
-  cal(1, 2020);
-  cal(2, 2020);
+  for (int i = 1; i <= 12; i++) {
+    cal(i, 2017);
+    printf("\n");
+  }
   return 0;
 }
--- cal/cal.h
+++ cal/cal.h
@@ -1,6 +1,9 @@
 #define TRUE 1
 #define FALSE 0
 
+#define CAL_WIDTH 20
+#define WEEK_DAYS 7
+
 int isLeapYear(int year) {
   /**
    * Algorithm for determining a leap year:
@@ -27,31 +30,56 @@ int getStartDay(int month, int year) {
    * Algorithm for computing day of Gregorian Calendar:
    * https://cs.uwaterloo.ca/~alopez-o/math-faq/node73.html
    */
-
+  int startDay;
   int century = (int) year / 100;
   year = month < 3 ? (year - (century * 100)) - 1 : (year - (century * 100)); // rolls if months are jan or feb
   month = month < 3 ? month + 10 : month - 2; // rolls over the month
 
-  return ((
+  startDay = (
     1 +
     ((int) (2.6 * month - 0.2)) -
     (2 * century) +
     year +
     ((int) year / 4) +
     ((int) century / 4)
-  ) % 7);
+  ) % WEEK_DAYS;
+
+  return startDay < 0 ? WEEK_DAYS + startDay : startDay;
+}
+
+void printMonth(int month) {
+  char *months[12] = {
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December"
+  };
+  int i, pad = (int) (CAL_WIDTH - strlen(months[month - 1])) / 2;
+
+  // Print left pad
+  for (i = 0; i < pad; i++) printf(" ");
+
+  printf("%s", months[month - 1]);
+
+  // Print right pad
+  for (i = 0; i < CAL_WIDTH - (pad + strlen(months[month - 1])); i++) printf(" ");
 }
 
 void cal(int month, int year) {
   int startDay = getStartDay(month, year);
+  int space, day;
+
+  printMonth(month);
+  printf("\n");
 
   // Print days of the week
   printf("Su Mo Tu We Th Fr Sa\n");
 
   // Print padded space
-  for (int space = 0; space < startDay; space++) printf("   ");
+  for (space = 0; space < startDay; space++) printf("   ");
 
-  for (int day = 1; day < getDaysOfMonth(month, year) + 1; day++, startDay++) {
+  // Print days
+  for (day = 1; day < getDaysOfMonth(month, year) + 1; day++, startDay++) {
     if (day < 10) printf(" ");
     printf("%i ", day);
     if ((startDay + 1) % 7 == 0) printf("\n");
